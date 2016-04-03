83452b3041a9a8c35b3ebc78f5e0c6de69f4d74e
Ryan Vice <ryan@vicesoftware.com>
2016-04-03 09:37:02 -0500

    Got full test coverage and refactored for readability and also to structure the code better for conceptst being covered.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# angular-1x-best-practices-sample
-Sample code from my Angular 1.x Best Practices and Lessons Learned talk
+# Angular 1.x Best Practices
+Sample code from my Angular 1.x Best Practices and Lessons Learned talk. In this talk I'm covering best practices that I've used successfully with large distributed teams on large code bases for high volume sites over long periods of time.
+
+## Running Code
+
+1. Clone repository or download zip file and extract it.
+1. Start a local server by executing ```http-server```
+1. Browse to IP and port created by http-server, http://127.0.0.1:8080/ on my machine
+
+## Running Tests
+
+1. ```karma start```
+
+## Session Notes
+1. Debugging Tips
+	* Learn to use chrome for development
+		* Workspaces
+		* Debugging using ```angular.element($0).scope()```
+1. Find a good starting point. We like John Papa: https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
+	* Branch: john-papa
+    
+		```git checkout john-papa```
+1.
--- tests.js
+++ tests.js
@@ -1,3 +1,6 @@
+/////////////////////////////////////////////////
+// Blog Controller
+/////////////////////////////////////////////////
 describe('BlogController ', function() {
 	beforeEach(module('app'));
 
@@ -12,32 +15,86 @@ describe('BlogController ', function() {
 
 	describe('on init ', function() {
 		it('$scope.userName, isBusy and userToFind should be falsy and posts should be an emtpy array', function() {
-			expect($scope.userName).toBeFalsy();
-			expect($scope.userToFind).toBeFalsy();
-			expect($scope.isBusy).toBeFalsy();
-			expect($scope.posts).toBeDefined();
-			expect($scope.posts.length).toBe(0);
+			expect($scope.model.userName).toBeFalsy();
+			expect($scope.model.userToFind).toBeFalsy();
+			expect($scope.model.isBusy).toBeFalsy();
+			expect($scope.model.posts).toBeDefined();
+			expect($scope.model.posts.length).toBe(0);
 		});
 	});
 
 	describe('when searching ', function() {
 		it('isBusy should toggle correctly', function() {
-			// var httpBackend = $injector.get('$httpBackend');
 			$httpBackend.expectGET('http://jsonplaceholder.typicode.com/users')
 				.respond([{id: 1}]);
 			$httpBackend.expectGET('http://jsonplaceholder.typicode.com/posts?userid=1')
 				.respond({});
 
-			expect($scope.isBusy).toBeFalsy();
+			expect($scope.model.isBusy).toBeFalsy();
 
 			$scope.search();
 
-			expect($scope.isBusy).toBeTruthy();
+			expect($scope.model.isBusy).toBeTruthy();
 
 			$httpBackend.flush();
 
-			expect($scope.isBusy).toBeFalsy();
+			expect($scope.model.isBusy).toBeFalsy();
 		});
+
+		it('isBusy should toggle correctly', function() {
+			var expectedUser = 'expectedUser';
+			$scope.model = { 
+				userToFind: expectedUser,
+				posts: []
+			};
+
+			var expectedUserId = 1;
+			var expectedPosts = [{ foo: 'bar' }];
+
+			$httpBackend.expectGET('http://jsonplaceholder.typicode.com/users?username=' + expectedUser)
+				.respond([{id: expectedUserId}]);
+			$httpBackend.expectGET('http://jsonplaceholder.typicode.com/posts?userid=' + expectedUserId)
+				.respond(expectedPosts);
+
+			$scope.search();
+
+			expect($scope.model.isBusy).toBeTruthy();
+
+			$httpBackend.flush();
+
+			expect($scope.model.posts).toEqual(expectedPosts);
+		});
+	});
+
+});
+
+/////////////////////////////////////////////////
+// Blog Controller
+/////////////////////////////////////////////////
+describe('BlogSearchBox ', function() {
+	beforeEach(module('app'));
+
+	var sut, $compile, $rootScope;
+
+	beforeEach(inject(function(_$compile_, _$rootScope_){
+		$compile = _$compile_;
+		$rootScope = _$rootScope_;
+	}));
+
+	it('Replaces the element with the appropriate content', function() {
+		var $scope = $rootScope.$new();
+		var expectedUserName = "expectedUser";
+		$scope.userName = expectedUserName;
+
+	    // Compile a piece of HTML containing the directive
+	    var element = $compile("<blog-search-box></blog-search-box>")($scope);
+	    // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+	    $scope.$digest();
+
+	    // Check that the compiled element contains the templated content
+	    var label = element[0].querySelector('label');
+	    expect(label.innerHTML)
+	    	.toBe("Posts by " + expectedUserName);
 	});
 
 });
