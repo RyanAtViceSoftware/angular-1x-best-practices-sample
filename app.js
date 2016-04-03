8e046927d03515a25c137148434b1a062e034ef3
Ryan Vice <ryan@vicesoftware.com>
2016-04-03 11:09:54 -0500

    Refactored to John Papa controllerAs

--- app.js
+++ app.js
@@ -3,33 +3,33 @@
 
 	var app = angular.module('app', []);
 
-	app.controller('BlogController', function ($scope, $http) {
-		$scope.model = {
-			userName: "",
-			posts: [],
-			isBusy: false,
-			error: null
-		};
+	app.controller('BlogController', function ($http) {
+		var vm = this; // safe to use in callbacks
+		
+		vm.userName = "";
+		vm.posts = [],
+		vm.isBusy = false;
 		
 
-		$scope.search = function() {
-			$scope.model.isBusy = true;
-			$scope.model.error = null;
-			
+		vm.search = function() {
+			vm.isBusy = true;
+
 			$http.get(
 				'http://jsonplaceholder.typicode.com/users', 
-				{ params: { username: $scope.model.userToFind}})
+				{ params: { username: vm.userToFind}})
 				.then(function(user) {
 					$http.get(
 						'http://jsonplaceholder.typicode.com/posts', 
 						{ params: {userid: user.data[0].id}})
 					.then(function(posts) {
-						$scope.model.posts.length = 0;
-						$scope.model.posts = posts.data;
-
-						$scope.model.isBusy = false;
+						vm.posts.length = 0;
+						vm.posts = posts.data;
+						vm.model.isBusy = false;
 					})
 					.catch(handleError);
+
+						vm.isBusy = false;
+					});
 				})
 				.catch(handleError);
 		};
@@ -44,12 +44,20 @@
 	app.directive('blogSearchBox', function() {
 		return {
 			restrict: 'E',
+			scope: {},
+			bindToController: {
+				userName: "=",
+				onSearch: "&",
+				isBusy: "="
+			},
+			controllerAs: "blogSearchBoxVm",
+			controller: function() {},
 			template: '<div class="jumbotron">' +
-			'<label for="searchBox">{{"Posts by " + userName}}</span></label>' +
+			'<label for="searchBox">{{"Posts by " + blogSearchBoxVm.userName}}</span></label>' +
 			'<div class="input-group">' +
-				'<input name="searchBox" type="text" class="form-control" placeholder="Find posts by..." ng-model="model.userToFind">' +
+				'<input name="searchBox" type="text" class="form-control" placeholder="Find posts by..." ng-model="blogSearchBoxVm.userName">' +
 				'<span class="input-group-btn">' +
-					'<button class="btn btn-primary" type="button" ng-click="search()">' +
+					'<button class="btn btn-primary" type="button" ng-click="blogSearchBoxVm.onSearch()">' +
 						'<span ng-show="model.isBusy" class="glyphicon glyphicon-refresh glyphicon-refresh-animate" aria-hidden="true"></span>' +
 						'<span ng-hide="model.isBusy" class="glyphicon glyphicon-search" aria-hidden="true"></span>' +
 					'</button>' +
