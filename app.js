21ded8802f046450fe1fdc0e1297cbcc7fcfcd35
Ryan Vice <ryan@vicesoftware.com>
2016-04-04 10:11:38 -0500

    Got stateful service refactor working but some tests are failing

--- app.js
+++ app.js
@@ -1,41 +1,107 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('app', []);
-
 	////////////////////////////////////////////////////
 	// BlogController
 	////////////////////////////////////////////////////
-	app.controller('BlogController', BlogController);
+	angular.module('app', []).controller('BlogController', BlogController);
 
-	BlogController.$inject = ['$http'];
+	BlogController.$inject = ['blog'];
 
-	function BlogController($http) {
-		var vm = this; // safe to use in callbacks
-		
-		vm.userName = "";
-		vm.posts = [],
-		vm.isBusy = false;
-		vm.search = search;
-		vm.error = null;
+	function BlogController(blog) {
+		// nothing interesting to test here
+		this.model = blog.model;
+		this.search = blog.search;
+	}
+})();
+
+	////////////////////////////////////////////////////
+	// Blog - Stateful Service (VM, Domaain Model)
+	////////////////////////////////////////////////////
+(function() {
+	'use strict';
+	angular.module('app').factory('blog', blog);
+
+	blog.$inject = ['$http', 'users', 'posts'];
+
+	function blog($http, users, posts) {
+		var service = {
+			model: {
+				userName: "",
+				posts: [],
+				isBusy: false,
+				search: search,
+				error: null
+			},
+			search: search
+		}
+
+		return service;
 
 		function search() {
-			vm.isBusy = true;
+			service.model.isBusy = true;
+			service.model.error = null;
 			
-			getUser(vm.userToFind)
-				.then(getPostsByUser)
+			users.getUser(service.model.userToFind)
+				.then(posts.getPostsByUser)
 				.then(updateModel)
 				.catch(handleError);
 		}
 
-		function getUser(userName) {
-			vm.error = null;
+		function updateModel(posts) {
+			service.model.posts.length = 0;
+			service.model.posts = posts.data;
 
+			service.model.isBusy = false;
+		}
+
+		function handleError(error) {
+			service.model.error = error.statusText;
+			service.model.isBusy = false;
+		}
+	}
+})();
+
+	////////////////////////////////////////////////////
+	// Users - Data Service - Wraps API calls
+	////////////////////////////////////////////////////
+(function() {
+	'use strict';
+	angular.module('app').factory('users', users);
+
+	users.$inject = ['$http'];
+
+	function users($http) {
+		var service = {
+			getUser: getUser
+		}
+
+		return service;
+
+		function getUser(userName) {
 			// return the promise to allow promise chaining
 			return $http.get(
 				'http://jsonplaceholder.typicode.com/users', 
 				{ params: { username: userName}});
 		}
+	}
+})();
+
+	////////////////////////////////////////////////////
+	// Posts - Data Service - Wraps API calls
+	////////////////////////////////////////////////////
+(function() {
+	'use strict';
+	angular.module('app').factory('posts', posts);
+
+	posts.$inject = ['$http'];
+
+	function posts($http) {
+		var service = {
+			getPostsByUser: getPostsByUser
+		}
+
+		return service;
 
 		function getPostsByUser(user) {
 			// return the promise to allow promise chaining
@@ -43,24 +109,15 @@
 					'http://jsonplaceholder.typicode.com/posts', 
 					{ params: {userid: user.data[0].id}});
 		}
-
-		function updateModel(posts) {
-			vm.posts.length = 0;
-			vm.posts = posts.data;
-
-			vm.isBusy = false;
-		}
-
-		function handleError(error) {
-			vm.error = error.statusText;
-			vm.isBusy = false;
-		}
 	}
+})();
 
 	////////////////////////////////////////////////////
 	// blogSearchBox
 	////////////////////////////////////////////////////
-	app.directive('blogSearchBox', blogSearchBox);
+(function() {
+	'use strict';
+	angular.module('app').directive('blogSearchBox', blogSearchBox);
 
 	function blogSearchBox() {
 		return {
